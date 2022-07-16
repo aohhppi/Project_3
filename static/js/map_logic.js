5ea08449a62b53885e77df1cf4a8ea34e5277672
aohhppi <98566990+aohhppi@users.noreply.github.com>
2022-07-15 23:30:16 -0400

    Update map_logic.js

--- static/js/map_logic.js
+++ static/js/map_logic.js
@@ -19,19 +19,79 @@ Atlanta = [-84.5606895, 33.7676338]
 
 var map = new ol.Map({
     target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
+    // layers: [
+    //   new ol.layer.Tile({
+    //     source: new ol.source.OSM()
+    //   })
+    // ],
     view: new ol.View({
       center: ol.proj.fromLonLat(Atlanta),
       zoom: 5.25,
-      maxZoom: 10,
-      minZoom: 2
+      maxZoom: 8,
+      minZoom: 3
     })
   });
 
+  // Base map layers group
+
+  var openStreetMapStandard = new ol.layer.Tile({
+    source: new ol.source.OSM(),
+    visible: true,
+    title: 'OSMStandard'
+  })
+
+  // var openStreetMapHumanitarian = new ol.layer.Tile({
+ 
+  //   source: new ol.source.OSM({
+  //     url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+  //   }),
+  //   visible: false,
+  //   title: 'OSMHumanitarian'
+  // })
+
+  var stamenTerrain = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: 'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+      attribuitions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    visible: false,
+    title: 'StamenTerrain'
+  })
+
+  // map.addLayer(openStreetMapStandard)
+
+  // add Layer Group
+  var baseLayerGroup = new ol.layer.Group({
+    layers: [
+      openStreetMapStandard, stamenTerrain
+    ]
+  })
+
+  map.addLayer(baseLayerGroup);
+
+// Layer switcher logic for basemaps
+  var baseLayerElements = document.querySelectorAll('.sidebar > input[type=radio]');
+  // console.log(baseLayerElements);
+  for(let baseLayerElement of baseLayerElements){
+    // console.log(baseLayerElement);
+    baseLayerElement.addEventListener('change', function(){
+      // console.log(this);
+      let baseLayerElementValue = this.value;
+      baseLayerGroup.getLayers().forEach(function(element, index, array){
+        // console.log(element.get('title'));
+        let baseLayerTitle = element.get('title');
+        element.setVisible(baseLayerTitle === baseLayerElementValue);
+  //     // })
+  //     // let baseLayerElementValue = this.value;
+  //     // baseLayerGroup.getLayers().forEach(function(element, index, array){
+  //     //   console.log(element.get('title'));
+      })
+
+    })
+  }
+  
+  
+  
   var fillStyle = new ol.style.Fill({
     color: [84, 118, 255, 1]
   })
@@ -47,7 +107,7 @@ var map = new ol.Map({
     }),
     radius: 7,
     stroke: strokeStyle
-  })
+  }) 
 
   var mapping = new ol.layer.Vector({
     source: new ol.source.Vector({
